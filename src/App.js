cd212fbdfdc45c7e2bef9a3a209e9a314ef76bfd
Venugopalan Iyengar <iyengarvenugopalan@gmail.com>
2022-08-08 11:17:36 +0530

    Converted to React Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import './App.css';
@@ -12,48 +12,55 @@ import ErrorBoundary from "./ErrorBoundary";
     The parent sends the state and the child receives the props i.e. robots
 */
 
-class App extends Component{
-    constructor(){
-        super()
-        //because this app has a state this is a smart component
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }    
-    }
+function App(){
+    // constructor(){
+    //     super()
+    //     //because this app has a state this is a smart component
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }    
+    // }
 
-    componentDidMount(){
+    //hooks are very react specific
+    //the states are now separate
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(res=>res.json())
+    //     .then(users=>this.setState({robots: users}))
+    // }
+
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res=>res.json())
-        .then(users=>this.setState({robots: users}))
+            .then(res=>res.json())
+            .then(users=>setRobots(users))
+    },[])
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)       
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})       
-    }
-
-    render(){
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        }) 
-        if(this.state.robots.length===0){
-            return <h1>Loading</h1>
-        }else{
-            return(
-                <div className="tc">
-                    <h1 className="f2">Robo Friends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots}/>
-                        </ErrorBoundary>
-                    </Scroll>   
-                </div> 
-            );
-        }   
-        
-    }
-
+    const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    }) 
+    if(robots.length===0){
+        return <h1>Loading</h1>
+    }else{
+        return(
+            <div className="tc">
+                <h1 className="f2">Robo Friends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundary>
+                </Scroll>   
+            </div> 
+        );
+    }   
 }
 
 export default App;
