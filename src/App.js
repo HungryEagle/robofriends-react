4683b5c22997dbc7fa3357e2902d94a248c25f6f
Venugopalan Iyengar <iyengarvenugopalan@gmail.com>
2022-08-08 09:34:27 +0530

    Robofriends app done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
 import './App.css';
+import Scroll from "./Scroll";
+import ErrorBoundary from "./ErrorBoundary";
+//state -> description of your app
+//Props never change.
+//props are simply things come out of the state. The child when receives
+//the state it gets the props out of it. 
+/*
+    The parent sends the state and the child receives the props i.e. robots
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(){
+        super()
+        //because this app has a state this is a smart component
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }    
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(users=>this.setState({robots: users}))
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})       
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        }) 
+        if(this.state.robots.length===0){
+            return <h1>Loading</h1>
+        }else{
+            return(
+                <div className="tc">
+                    <h1 className="f2">Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
+                    </Scroll>   
+                </div> 
+            );
+        }   
+        
+    }
+
 }
 
 export default App;
---
+++ src/CardList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './card';
+
+const CardList = ({robots}) =>{
+    // if(true){
+    //     throw new Error('NOOOOOOOO!');
+    // }
+    return(
+    <div>
+        {
+            robots.map((user,i)=>{
+                return (
+                    <Card 
+                    key={i}
+                    id={robots[i].id} 
+                    name={robots[i].name} 
+                    email={robots[i].email} />
+                );
+            })
+        }
+    </div>
+    );
+}
+
+export default CardList;
